2eb35bd2a02342e3c66decc3a4100561ed99308a
Mark Hesselberth - de Hond <mark.hesselberth@youngones.works>
2024-10-17 09:21:08 +0200

    Add additional Firebase Analytics consent enum values

--- source/Firebase/Analytics/Enums.cs
+++ source/Firebase/Analytics/Enums.cs
@@ -6,23 +6,31 @@ using ObjCRuntime;
 
 namespace Firebase.Analytics
 {
-    public enum ConsentType {
+	public enum ConsentType {
 		// extern FIRConsentType const FIRConsentTypeAdStorage;
 		[Field ("FIRConsentTypeAdStorage", "__Internal")]
 		AdStorage,
 
-        // extern FIRConsentType const FIRConsentTypeAnalyticsStorage;
+		// extern FIRConsentType const FIRConsentTypeAnalyticsStorage;
 		[Field ("FIRConsentTypeAnalyticsStorage", "__Internal")]
 		AnalyticsStorage,
-    }
 
-    public enum ConsentStatus {
+		// extern FIRConsentType const FIRConsentTypeAdUserData;
+		[Field ("FIRConsentTypeAdUserData", "__Internal")]
+		AdUserData,
+
+		// extern FIRConsentType const FIRConsentTypeAdPersonalization;
+		[Field ("FIRConsentTypeAdPersonalization", "__Internal")]
+		AdPersonalization,
+	}
+
+	public enum ConsentStatus {
 		// extern FIRConsentStatus const FIRConsentStatusDenied;
 		[Field ("FIRConsentStatusDenied", "__Internal")]
 		Denied,
 
-        // extern FIRConsentStatus const FIRConsentStatusGranted;
+		// extern FIRConsentStatus const FIRConsentStatusGranted;
 		[Field ("FIRConsentStatusGranted", "__Internal")]
 		Granted,
-    }
+	}
 }
