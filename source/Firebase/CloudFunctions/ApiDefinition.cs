3f8c919606e4705acbd161926358f15c0ae62737
Adam Essenmacher <adam.essenmacher@gmail.com>
2026-04-16 17:09:42 -0400

    Fix Firebase CloudFunctions binding surface (#164)

--- source/Firebase/CloudFunctions/ApiDefinition.cs
+++ source/Firebase/CloudFunctions/ApiDefinition.cs
@@ -47,14 +47,39 @@ namespace Firebase.CloudFunctions
 		[Export("HTTPSCallableWithName:")]
 		HttpsCallable HttpsCallable(string name);
 
+		//- (FIRHTTPSCallable *)HTTPSCallableWithName:(NSString *)name options:(FIRHTTPSCallableOptions *)options;
+		[Export("HTTPSCallableWithName:options:")]
+		HttpsCallable HttpsCallable(string name, HttpsCallableOptions options);
+
+		//- (FIRHTTPSCallable *)HTTPSCallableWithURL:(NSURL *)url;
+		[Export("HTTPSCallableWithURL:")]
+		HttpsCallable HttpsCallable(NSUrl url);
+
+		//- (FIRHTTPSCallable *)HTTPSCallableWithURL:(NSURL *)url options:(FIRHTTPSCallableOptions *)options;
+		[Export("HTTPSCallableWithURL:options:")]
+		HttpsCallable HttpsCallable(NSUrl url, HttpsCallableOptions options);
+
 		//- (void)useEmulatorWithHost:(NSString *)host port:(NSInteger) port;
 		[Export ("useEmulatorWithHost:port:")]
-		void UseEmulatorOriginWithHost (string host, uint port);
+		void UseEmulatorWithHost (string host, nint port);
 	}
 
 	// void (^)(FIRHTTPSCallableResult *_Nullable result, NSError *_Nullable error);
 	delegate void HttpsCallableResultHandler([NullAllowed] HttpsCallableResult result, [NullAllowed] NSError error);
 
+	[DisableDefaultCtor]
+	[BaseType(typeof(NSObject), Name = "FIRHTTPSCallableOptions")]
+	interface HttpsCallableOptions
+	{
+		//- (instancetype)initWithRequireLimitedUseAppCheckTokens:(BOOL)requireLimitedUseAppCheckTokens;
+		[DesignatedInitializer]
+		[Export("initWithRequireLimitedUseAppCheckTokens:")]
+		NativeHandle Constructor(bool requireLimitedUseAppCheckTokens);
+
+		//@property(nonatomic, readonly) BOOL requireLimitedUseAppCheckTokens;
+		[Export("requireLimitedUseAppCheckTokens")]
+		bool RequireLimitedUseAppCheckTokens { get; }
+	}
 
 	[DisableDefaultCtor]
 	[BaseType(typeof(NSObject), Name = "FIRHTTPSCallable")]
