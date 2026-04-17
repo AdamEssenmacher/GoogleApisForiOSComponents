078137e53adacb1c097621b2551e009f51a9784b
Adam Essenmacher <adam.essenmacher@gmail.com>
2026-04-17 19:02:45 -0400

    Harden Firebase version string caches (#169)

--- source/Firebase/CloudFunctions/Extension.cs
+++ source/Firebase/CloudFunctions/Extension.cs
@@ -3,14 +3,24 @@ using System.Runtime.InteropServices;
 using ObjCRuntime;
 namespace Firebase.CloudFunctions {
 	public partial class CloudFunctions {
-		static string currentVersion;
+		static string? currentVersion;
 		public static string CurrentVersion {
 			get {
 				if (currentVersion == null) {
 					IntPtr RTLD_MAIN_ONLY = Dlfcn.dlopen (null, 0);
-					IntPtr ptr = Dlfcn.dlsym (RTLD_MAIN_ONLY, "FirebaseCloudFunctionsVersionStr");
-					currentVersion = Marshal.PtrToStringAnsi (ptr);
-					Dlfcn.dlclose (RTLD_MAIN_ONLY);
+					if (RTLD_MAIN_ONLY == IntPtr.Zero)
+						throw new InvalidOperationException ("Unable to open the main program handle.");
+
+					try {
+						IntPtr ptr = Dlfcn.dlsym (RTLD_MAIN_ONLY, "FirebaseCloudFunctionsVersionStr");
+						if (ptr == IntPtr.Zero)
+							throw new InvalidOperationException ("Unable to resolve FirebaseCloudFunctionsVersionStr.");
+
+						currentVersion = Marshal.PtrToStringAnsi (ptr)
+							?? throw new InvalidOperationException ("Unable to read FirebaseCloudFunctionsVersionStr.");
+					} finally {
+						Dlfcn.dlclose (RTLD_MAIN_ONLY);
+					}
 				}
 
 				return currentVersion;
